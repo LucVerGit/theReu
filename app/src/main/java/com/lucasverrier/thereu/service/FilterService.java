16d5fb6e677abe652ad994b1ef717c8c5259fa92
byebyebabayaga <lucasverrier1@gmail.com>
2020-10-21 11:54:11 +0200

    filter service update
    add filter for list for clear view

--- app/src/main/java/com/lucasverrier/thereu/service/FilterService.java
+++ app/src/main/java/com/lucasverrier/thereu/service/FilterService.java
@@ -1,7 +1,21 @@
 package com.lucasverrier.thereu.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilterService implements FilterApiService {
- 
+
+    public List<Meeting> placeFilter(List<Meeting> meetingList, String place) {
+        List<Meeting> toRemoveList = new ArrayList<>();
+        for (Meeting meeting : meetingList) {
+            if (!meeting.getMeetingPlace().equals(place)) {
+                toRemoveList.add(meeting);
+            }
+        }
+        meetingList.removeAll(toRemoveList);
+        return meetingList;
+    }
+
 
 
 }
