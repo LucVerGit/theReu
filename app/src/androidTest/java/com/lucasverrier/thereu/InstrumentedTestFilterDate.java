b2cbe4cc1b57611260da1f6c7e7ce05723bc0625
byebyebabayaga <lucasverrier1@gmail.com>
2020-10-27 17:27:05 +0100

    init setups

--- app/src/androidTest/java/com/lucasverrier/thereu/InstrumentedTestFilterDate.java
+++ app/src/androidTest/java/com/lucasverrier/thereu/InstrumentedTestFilterDate.java
@@ -11,14 +11,27 @@ import com.lucasverrier.thereu.service.MeetingApiService;
 import org.joda.time.LocalDateTime;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 
 import java.util.ArrayList;
 
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static com.lucasverrier.thereu.utils.RecyclerViewItemCountAssertion.withItemCount;
 
 public class InstrumentedTestFilterDate {
 
@@ -47,5 +60,21 @@ public class InstrumentedTestFilterDate {
         service.addNewMeeting(nextMonth);
     }
 
+    @Test
+    public void dateFilterAction_today(){
+        //Click on filter tab item
+        onView(withId(R.id.action_filter)).perform(click());
+        onView(withId(R.id.card_filter)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        //Click on spinner item "Today"
+        onView(withId(R.id.date_spinner)).perform(click());
+        onData(is(res.getString(R.string.date_spinner_today))).perform(click());
+        //Check if the correct meeting is displayed
+        onView(withId(R.id.list_meeting_container)).check(withItemCount(1));
+        onView(withText("Today")).check(matches(isDisplayed()));
+        onView(withText("Tomorrow")).check(doesNotExist());
+        onView(withText("Next Week")).check(doesNotExist());
+        onView(withText("Next Month")).check(doesNotExist());
+    }
 
 }
--- app/src/test/java/com/lucasverrier/thereu/UnitTestFilterDateService.java
+++ app/src/test/java/com/lucasverrier/thereu/UnitTestFilterDateService.java
@@ -1,4 +1,25 @@
 package com.lucasverrier.thereu;
 
+import com.lucasverrier.thereu.model.Meeting;
+import com.lucasverrier.thereu.service.DI;
+import com.lucasverrier.thereu.service.FilterApiService;
+import com.lucasverrier.thereu.service.MeetingApiService;
+
+import org.joda.time.LocalDateTime;
+import org.junit.Before;
+
+import java.util.ArrayList;
+
 public class UnitTestFilterDateService {
+    MeetingApiService meetingService;
+    FilterApiService filterService;
+
+    @Before
+    public void setup() {
+        meetingService = DI.getMeetingApiService();
+        meetingService.addNewMeeting(new Meeting("Today", "Mario", LocalDateTime.now(), 1, new ArrayList<>()));
+        meetingService.addNewMeeting(new Meeting("Tomorrow", "Mario", LocalDateTime.now().plusDays(1), 2, new ArrayList<>()));
+        meetingService.addNewMeeting(new Meeting("Next Week", "Mario", LocalDateTime.now().plusDays(8), 3, new ArrayList<>()));
+        meetingService.addNewMeeting(new Meeting("Next Month", "Mario", LocalDateTime.now().plusMonths(1).plusDays(1), 4, new ArrayList<>()));
+    }
 }
